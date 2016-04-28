3e9fd8c02d7553056ae38362b387a3f89646db1f
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-04-27 22:09:19 -1000

    added error checking when printing unicode characters in Python 2

--- tinderp.py
+++ tinderp.py
@@ -10,7 +10,7 @@ def get_matches(session):
     matches = [m.user.dict() for m in session.matches(filter_empty_matches=True)]
     matches["_id"] = matches.pop("id")
 
-def get_hopefuls(session, limit=10):
+def get_hopefuls(session, limit=10, verbose=False):
     """
     Return a list of user dictionaries from hopefuls.
 
@@ -20,7 +20,15 @@ def get_hopefuls(session, limit=10):
     hopefuls = session.nearby_users(limit=limit)
     for hopeful in hopefuls:
         hopeful.like()
-        print("Just liked:", hopeful)
+
+    if verbose:
+        for hopeful in hopefuls:
+            try:
+                print("Just liked:", hopeful)
+            except UnicodeEncodeError as e:
+                hopeful = unicode(hopeful.__str__(), errors='ignore')
+                print("Just liked:", hopeful)
+
 
     for hopeful in hopefuls:
         d = hopeful.dict()
@@ -35,18 +43,24 @@ def persist(collection, user):
 if __name__ == "__main__":
     from pprint import pprint
     from pymongo import MongoClient
+    from six.moves import configparser
+    import os
 
-    session = pynder.Session("1165230333",
-                             "CAAGm0PX4ZCpsBAIXjO1ARhwBjiUtQpMc1JfKO8M82QIO8SVPmmZCCX0ZBYhBnV8689dRlDRsAGEWsomBj858o0OC3ZCqG0tCRm6ZA8IuRbHiQ9AxxJyHyFJwR2eh20xEpOZAyZAAaJ684JKhQpmX3ZBUAmHGWDfFAyzk3Aepuro9VUW7v18QaXgJ6ZCULxXy43HHBHBwMUOsR5kNi9l3V1HDRsBRtvmuVZABLtiL8ZCnbe8DCXuBK6NkePk")
+    config = configparser.ConfigParser()
+    config.read(os.path.expanduser("~/.api_tokens.conf"))
+    user_id = config.get("tinder", "id")
+    token = config.get("tinder", "token")
+
+    session = pynder.Session(user_id, token)
     client = MongoClient()
     db = client.tinderp
     match_collection = db.matches
     hopefuls_collection = db.hopefuls
 
-    hopefuls = get_hopefuls(session, limit=100)
+    hopefuls = get_hopefuls(session, limit=100, verbose=True)
     while hopefuls:
         print("liked {} people".format(len(hopefuls)))
         for hopeful in hopefuls:
             persist(hopefuls_collection, hopeful)
 
-        hopefuls = get_hopefuls(session, limit=100)
+        hopefuls = get_hopefuls(session, limit=100, verbose=True)
