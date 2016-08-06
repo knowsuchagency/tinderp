084b5870d180acf38394a517447659eed7b993c8
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2016-08-06 14:30:52 -0700

    added readme; other changes to code

--- tinderp/settings.py
+++ tinderp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'photologue',
     'sortedm2m',
 ]
@@ -121,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
+
+# Sites settings
+
+SITE_ID = 1
